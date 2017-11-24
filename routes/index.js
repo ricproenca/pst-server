866fd8c2a80f0b9d032813650ad029d88e9ba8a4
Ricardo Proenca <ext.ricardo.proenca@ingg.com>
2017-11-24 16:45:41 +0000

    testing heroku

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    project: 'pst-server',
+    project: 'pst-server-api',
     title: 'Project School Timetables API',
     description: 'for automatic generation of timetables for schools'
   });
