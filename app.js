2ef2d857f85882b828c61659a2467e7dd08dbff7
Ricardo Proenca <ext.ricardo.proenca@ingg.com>
2017-11-24 16:39:38 +0000

    added procfile

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
@@ -20,14 +19,15 @@ app.use(helmet());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator()); // Add this after the bodyParser middleware!
 
-app.use(compression()); //Compress all routes
+app.use(expressValidator());
+
+//Compress all routes
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
