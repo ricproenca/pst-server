cc06f4512bc2e5c13e6842d8efffee50aa85b45a
Ricardo Proenca <ext.ricardo.proenca@ingg.com>
2017-11-24 17:23:40 +0000

    heroku is working

--- app.js
+++ app.js
@@ -13,41 +13,47 @@ import helmet from 'helmet';
 // Create the Express application object
 const app = express();
 
+// Add various HTTP headers to improve app security
 app.use(helmet());
 
-// view engine setup
+// Setup View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// HTTP request logger middleware
 app.use(logger('dev'));
 
+// Body parsing middleware.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Express middleware request data validator
 app.use(expressValidator());
 
-//Compress all routes
+// Routes compression middleware
 app.use(compression());
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setup routes
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
+  // Locally set locals and provide the error
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
