8a96c05f0739ae399f7511a7893224839cc435a4
Ricardo Proença <ricproenca@gmail.com>
2017-11-26 01:51:57 +0000

    fixed test

--- app/api/v1/tests/index.test.js
+++ app/api/v1/tests/index.test.js
@@ -1,16 +1,26 @@
 // app/api/v1/tests/index.test.js
 'use strict';
 
-import http from 'http';
-import assert from 'assert';
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../../../init/server');
 
-import '../app';
+let should = chai.should();
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:4000', res => {
-      assert.equal(200, res.statusCode);
-      done();
+chai.use(chaiHttp);
+
+describe('Users', () => {
+  describe('/GET users', () => {
+    it('it should GET all the users', done => {
+      chai.request(server).get('/users').end((err, res) => {
+        res.should.have.status(200);
+        res.text.should.equal('should get users');
+        done();
+      });
     });
   });
+});
+
+after(() => {
+  process.exit(0);
 });
