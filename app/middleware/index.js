367b03530c12475245b0f38ed8bcb8cbc40ab5aa
Ricardo Proença <ricproenca@gmail.com>
2017-11-26 00:57:09 +0000

    add color to logs

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -3,8 +3,38 @@ import bodyParser from 'body-parser';
 import expressValidator from 'express-validator';
 import compression from 'compression';
 import helmet from 'helmet';
+import chalk from 'chalk';
 
 import appConfig from '../config/app';
+
+const padLeft = (str, len) => {
+  return len > str.length
+    ? new Array(len - str.length + 1).join(' ') + str
+    : str;
+};
+
+const padRight = (str, len) => {
+  return len > str.length
+    ? str + new Array(len - str.length + 1).join(' ')
+    : str;
+};
+
+const formatMorganString = (tokens, req, res) => {
+  var status = tokens.status(req, res);
+  var statusColor = status >= 500
+    ? 'red'
+    : status >= 400 ? 'yellow' : status >= 300 ? 'cyan' : 'green';
+
+  return (
+    chalk.reset(
+      padRight(tokens.method(req, res) + ' ' + tokens.url(req, res), 42)
+    ) +
+    ' ' +
+    chalk[statusColor](status) +
+    ' ' +
+    chalk.reset(padLeft(tokens['response-time'](req, res) + ' ms', 8))
+  );
+};
 
 module.exports = () => {
   return app => {
@@ -12,7 +42,11 @@ module.exports = () => {
     app.use(helmet());
 
     // HTTP request morgan middleware
-    app.use(morgan(appConfig[process.env.NODE_ENV].morgan));
+    let morganFormatter = appConfig[process.env.NODE_ENV].morgan === 'custom'
+      ? formatMorganString
+      : appConfig[process.env.NODE_ENV].morgan;
+
+    app.use(morgan(morganFormatter));
 
     // Body parsing middleware.
     app.use(bodyParser.json());
